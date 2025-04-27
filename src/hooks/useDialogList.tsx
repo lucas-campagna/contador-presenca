4bb868c02a741a3d2448b88f6c6a559885119891
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-04-27 15:32:30 -0300

    fix: dialog list items key

--- src/hooks/useDialogList.tsx
+++ src/hooks/useDialogList.tsx
@@ -23,13 +23,14 @@ function DialogList({ title, items }: DialogListProps) {
         <div className="flex flex-col justify-start items-strech p-3 bg-white rounded-sm shadow-sm gap-3">
             {title && <div className="flex items-center font-bold text-xl">{title}</div>}
             {
-                Object.entries(items).map(([name, action], index) => (<div
-                    className="bg-sky-500 p-2 rounded-sm text-white"
-                    onClick={() => handleOnClick(name, action)}
-                    data-index={index}
-                >
-                    {name}
-                </div>))
+                Object.entries(items).map(([name, action], index) => (
+                    <div
+                        className="bg-sky-500 p-2 rounded-sm text-white"
+                        onClick={() => handleOnClick(name, action)}
+                        key={index}
+                    >
+                        {name}
+                    </div>))
             }
         </div>
     );
