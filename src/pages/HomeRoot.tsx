9ec4056b6bb32fba708edc786bf1dd76c0a6212e
Your Name <you@example.com>
2025-05-02 16:53:20 -0300

    feat: signout from header

--- src/pages/HomeRoot.tsx
+++ src/pages/HomeRoot.tsx
@@ -1,21 +1,39 @@
 import Header from "../components/Header";
 import useAuth from "../hooks/useAuth";
 import { Outlet } from "react-router";
+import useDialog from "../hooks/useDialog";
+import Button from "../components/Button";
 
 function HomeRoot() {
   const { isSignedIn, signOut } = useAuth();
+  const { setDialog, clearDialog } = useDialog();
 
   if (!isSignedIn) {
     return null;
   }
 
   function handleMenuClick() {
-    signOut();
+    setDialog(
+      <div className="flex flex-col gap-4">
+        <h1>Sair da conta?</h1>
+        <div className="flex flex-row gap-2">
+          <Button
+            text={"Sim"}
+            onClick={() => {
+              signOut();
+              clearDialog();
+            }}
+          />
+          <Button text={"Não"} onClick={() => clearDialog()} />
+        </div>
+      </div>
+    );
+    // signOut();
   }
 
   return (
     <>
-      <Header onMenuClick={handleMenuClick}/>
+      <Header onMenuClick={handleMenuClick} />
       <div className="flex flex-col items-stretch justify-start overflow-y-auto overflow-x-hidden w-full h-full p-2">
         <Outlet />
       </div>
