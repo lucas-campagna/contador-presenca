5075616358c3f01c0c433e64bb86a710a36d90e8
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-05-03 15:54:10 -0300

    feat: build url params in utls

--- src/utils.tsx
+++ src/utils.tsx
@@ -8,3 +8,12 @@ export function formatDate(date?: Date): string {
   date = date ?? new Date();
   return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 }
+
+export function buildURLParams(obj: any): string {
+  const params = new URLSearchParams();
+  for (const key in obj) {
+    const value = obj[key];
+    params.set(key, typeof value === 'string' ? value : JSON.stringify(value));
+  }
+  return params.toString();
+}
