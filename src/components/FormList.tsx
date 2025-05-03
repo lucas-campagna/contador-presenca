bc032158f6ef16e336fe25151c54b8f52c79d3e7
Your Name <you@example.com>
2025-05-03 13:04:34 -0300

    feat: create FormList

---
+++ src/components/FormList.tsx
@@ -0,0 +1,41 @@
+import Button from "./Button";
+
+// TODO: fix type
+// type AllowedFieldTypes = typeof Input | typeof Select;
+type FormFields = {
+  name: string;
+  type: any; //AllowedFieldTypes;
+  [key: string]: any;
+}; //& Parameters<AllowedFieldTypes>;
+type FormProps = {
+  title?: string;
+  fields: FormFields[];
+  onSubmit: CallableFunction;
+  submitText?: string;
+};
+
+function FormList({ title, fields, onSubmit, submitText }: FormProps) {
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    onSubmit(
+      fields.reduce(
+        (acc, { name }) => ({ ...acc, [name]: e.target.elements[name].value }),
+        {}
+      )
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-stretch gap-2">
+      {title && <div className="font-bold mb-2">{title}</div>}
+      <>
+        {fields.map(({ name, type: Type, ...props }) => (
+          <Type name={name} {...props} />
+        ))}
+      </>
+      <Button text={submitText ?? "Ok"} type="submit" />
+    </form>
+  );
+}
+
+export default FormList;
