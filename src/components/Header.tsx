d180f065179d7dccf57d2788f1adcfa145a08d83
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-05-03 15:55:54 -0300

    feat: left header button should get back if not in home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import { ThreeBarsIcon } from "@primer/octicons-react";
+import { JSX } from "react";
 
 type HeaderPropsType = {
     onMenuClick: () => void;
+    leftIcon?: JSX.Element;
 }
 
-const Header = ({ onMenuClick }: HeaderPropsType) => (
+const Header = ({ onMenuClick, leftIcon }: HeaderPropsType) => (
   <div className="flex flex-row items-center justify-between bg-primary w-full relative p-2 min-h-10">
     <div
       className="flex flex-row items-center justify-start text-white p-2 absolute left-0"
       onClick={onMenuClick}
     >
-      <ThreeBarsIcon size={24} />
+      {leftIcon || <ThreeBarsIcon size={24} />}
     </div>
   </div>
 );
--- src/pages/HomeRoot.tsx
+++ src/pages/HomeRoot.tsx
@@ -1,18 +1,27 @@
+import { ChevronLeftIcon } from '@primer/octicons-react'
 import Header from "../components/Header";
 import useAuth from "../hooks/useAuth";
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import useDialog from "../hooks/useDialog";
 import Button from "../components/Button";
+import { useMemo } from "react";
 
 function HomeRoot() {
   const { isSignedIn, signOut } = useAuth();
   const { setDialog, closeDialog } = useDialog();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isInHomePage = useMemo(() => location.pathname === '/', [location]);
 
   if (!isSignedIn) {
     return null;
   }
 
   function handleMenuClick() {
+    if (!isInHomePage) {
+      navigate(-1);
+      return
+    }
     setDialog(
       <div className="flex flex-col gap-4">
         <h1>Sair da conta?</h1>
@@ -32,7 +41,7 @@ function HomeRoot() {
 
   return (
     <>
-      <Header onMenuClick={handleMenuClick} />
+      <Header onMenuClick={handleMenuClick} leftIcon={isInHomePage ? undefined : <ChevronLeftIcon/>} />
       <div className="flex flex-col items-stretch justify-start overflow-y-auto overflow-x-hidden w-full h-full p-2">
         <Outlet />
       </div>
