2145f22e4cb2ad5d860d0a9dc12f576fcfcff6a1
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-05-03 18:59:59 -0300

    fix: Escola methods

--- src/models/escola.tsx
+++ src/models/escola.tsx
@@ -3,6 +3,8 @@ import { DocumentReference } from "firebase/firestore";
 import firestore, { DocumentRequest } from "./firestore";
 import Base from "./base";
 
+// TODO
+// @ts-ignore
 class Escola extends Base {
   nome = "";
 
@@ -31,6 +33,31 @@ class Escola extends Base {
     Escola._savedRef = firestore<DocumentRequest>(`escolas/${uid}`).ref;
     return Escola._savedRef;
   }
+
+  static async get<T extends typeof Escola, U extends Escola>(
+    this: T
+  ): Promise<U> {
+    return super.get(Escola.ref) as Promise<U>;
+  }
+
+  static async set<T extends typeof Escola, U extends Escola>(
+    this: T,
+    content: U
+  ) {
+    // TODO
+    // @ts-ignore
+    await this.create(content);
+  }
+
+  static async build<T extends typeof Escola, U extends Escola>(
+    this: T,
+    other: U
+  ): Promise<Escola> {
+    const r = await super.build(other);
+    r.ref = Escola.ref;
+    return r;
+  }
+
 
   async rename(nome: string) {
     await this.update({ nome } as Partial<Escola>);
