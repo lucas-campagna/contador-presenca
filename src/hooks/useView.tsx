4772d0faf4aad54db8bbbe48c37a8b492a16c367
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-05-03 19:01:17 -0300

    fix: useView prop type

--- src/hooks/useView.tsx
+++ src/hooks/useView.tsx
@@ -1,16 +1,9 @@
 import { useNavigate } from "react-router";
-import { FormProps } from "../components/FormList";
 import { buildURLParams } from "../utils";
-
-type OpenViewProps = {
-    title: string;
-    collection?: string;
-    fields: FormProps['fields'];
-}
 
 const useView = (name: string) => {
     const navigate = useNavigate();
-    return (obj: OpenViewProps) =>
+    return (obj: any) =>
         navigate(`/${name}?` + buildURLParams(obj));
 }
 
