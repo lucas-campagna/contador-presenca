8e4119e59a7d2ca97fd27d32de1a9aaa6828c923
Your Name <you@example.com>
2025-05-03 13:06:42 -0300

    feat: integrate models to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
+import "./models";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import PWABadge from "./PWABadge.tsx";
 import "./index.css";
 import App from "./App.tsx";
-import './models/escola.tsx'
-import './models/professor.tsx'
-import './models/firestore.tsx'
-import './models/classe.tsx'
-import './models/aluno.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/models/index.tsx
@@ -0,0 +1,7 @@
+// NOTE: must be here to export models to window and to resolve interdependency betwenn Base and Escola
+import './firestore.tsx'
+import './escola.tsx'
+import './base.tsx'
+import './professor.tsx'
+import './classe.tsx'
+import './aluno.tsx'
