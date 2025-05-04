a7857460d9201fb41e5eea26e5a3aae5c8dae143
Lucas Prett Campagna <lucasprett120@gmail.com>
2025-05-04 02:42:45 -0300

    feat: IN DEVELOPMENT
    this commit is been creater because the next one will modify its contet

--- src/views/Details.tsx
+++ src/views/Details.tsx
@@ -1,4 +1,4 @@
-import { JSX, lazy, memo, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router";
 import FormList from "../components/FormList";
 import Aluno from "../models/aluno";
@@ -6,32 +6,47 @@ import Classe from "../models/classe";
 import Professor from "../models/professor";
 import Input from "../components/Input";
 import { capitalize } from "../utils";
+import Button from "../components/Button";
+import useView from "../hooks/useView";
+import Base from "../models/base";
+import { InvalidType } from "../errors";
 
 type FieldType = {
     [key: string]: any;
 }
 
-type ComponentsType = {
-    [key: string]: JSX.Element
-};
-const Components: ComponentsType = ['Input', 'Select', 'Button']
-    .reduce((acc, cur) => ({
-        ...acc,
-        [cur]: memo(lazy(() => import(`../components/${cur}`)))
-    }), {});
+export type ParamsType = {
+    id: string;
+    collection: string;
+    title?: string;
+}
 
-const Collections = {
-    alunos: Aluno,
-    professores: Professor,
-    classes: Classe,
+type CollectionsType = {
+    collection?: string;
+    title?: string;
+    type?: typeof Base;
+}
+const Collections: CollectionsType[] = [
+    { collection: 'alunos', type: Aluno, title: 'aluno' },
+    { collection: 'professores', type: Professor, title: 'professor' },
+    { collection: 'classes', type: Classe, title: 'classe' },
+]
+const searchCollection = (s: CollectionsType): CollectionsType => (
+    s.collection ? Collections.filter(c => c.collection === s.collection)?.[0] :
+        s.type ? Collections.filter(c => c.type === s.type)?.[0] :
+            Collections.filter(c => c.title === s.title)?.[0]
+)
+function isModel(value: any) {
+    return Collections.some(({ type }) => type === value) ?? false;
 }
 
 function Details() {
     const [fields, setFields] = useState<FieldType[]>([]);
     const [editing, setEditing] = useState(false);
+    const [ref, setRef] = useState<any>(null);
     const [params, _] = useSearchParams();
     const navigate = useNavigate();
-    const [ref, setRef] = useState<any>(null);
+    const gotoDetailsView = useView('detalhes');
 
     const { title, id, collection } = useMemo(() => ({
         title: params.get('title') ?? '',
@@ -40,24 +55,68 @@ function Details() {
     }
     ), [params]);
 
-    const model = useMemo(() => Collections[collection as keyof typeof Collections], [collection]);
+    const Model = useMemo(() => searchCollection({ collection }).type, [collection]);
 
     useEffect(() => {
-        if (!id || !(collection in Collections)) {
+        if (!Model || !id || !Collections.some(({ collection: c }) => c === collection)) {
             navigate('/');
+            return;
         }
 
         async function fetchData() {
-            const doc = await model.get(id as string);
+            const doc = await (Model as typeof Base).get(id as string);
             if (!doc) {
                 navigate(-1);
+                return;
+            }
+            try {
+                await doc.pull();
+            } catch { }
+
+            function buildFields(value: any) {
+                if (typeof value === 'string') {
+                    return { value, type: Input };
+                }
+                if (Array.isArray(value)) {
+                    return {
+                        type: Button,
+                        _type: 'button',
+                        disabled: false,
+                        onClick: () => {
+
+                        }
+                    }
+                }
+                if (isModel(value)) {
+                    return {
+                        type: Button,
+                        _type: 'button',
+                        disabled: false,
+                        onClick: () => {
+                            const result = searchCollection({ type: value });
+                            if (!result) {
+                                navigate(-1);
+                                throw InvalidType();
+                            }
+                            gotoDetailsView({ title: result.title, id: value.ref.id, collection: result.collection })
+                        }
+                    }
+                }
+                if (typeof value === 'object') {
+                    return {
+                        type: Input,
+                        value: value.value ?? ''
+                    };
+                }
+                navigate(-1);
+                throw InvalidType(value);
             }
             const fields = Object.entries(doc)
                 .filter(([k]) => k !== 'ref')
                 .map(([name, value]) => ({
                     name: name.toLowerCase().trim().replace(/\s+/g, '-'),
                     label: capitalize(name),
-                    value,
+                    ...buildFields(value),
                 }));
             setRef(doc.ref);
             setFields(fields);
@@ -71,16 +130,22 @@ function Details() {
             setEditing(true);
             return;
         }
-        const m = await model.build({...data, ref});
+        const m = await (Model as typeof Base).build({ ...data, ref });
         await m.push();
         navigate(-1);
+    }
+
+
+    function handleCancel() {
+        setEditing(false);
     }
 
     return (
         <FormList
             title={title}
-            fields={fields.map((f: any) => ({ ...f, type: Input, disabled: !editing }))}
+            fields={fields.map((f: any) => ({ disabled: !editing, ...f }))}
             onSubmit={(id && collection) ? handleSubmit : undefined}
+            onCancel={editing ? handleCancel : undefined}
             submitText={editing ? "Salvar" : "Editar"}
         />
     );
